78018c1742da366d42cae785e4fa7e8426063c41
Viktor Diezel <viktor.diezel@ospin.de>
2022-06-03 14:09:28 +0200

    adds exports from the index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = {}
+const Workflow = require('./src/workflow/Workflow')
+const Validator = require('./src/workflow/validator/Validator')
+const EventListeners = require('./src/workflow/elements/eventListeners/EventListeners')
+const EventDispatchers = require('./src/workflow/elements/eventDispatchers/EventDispatchers')
+const Phases = require('./src/workflow/elements/phases/Phases')
+
+module.exports = {
+  Workflow,
+  Validator,
+  ElementsHandler: {
+    EventListeners,
+    EventDispatchers,
+    Phases,
+  },
+}
