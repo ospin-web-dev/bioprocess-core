c88dc56dfee775e4b7bfbe686e10c4219ebbcfe3
Viktor Diezel <viktor.diezel@ospin.de>
2022-06-07 15:21:24 +0200

    export gateways

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Validator = require('./src/workflow/validator/Validator')
 const EventListeners = require('./src/workflow/elements/eventListeners/EventListeners')
 const EventDispatchers = require('./src/workflow/elements/eventDispatchers/EventDispatchers')
 const Phases = require('./src/workflow/elements/phases/Phases')
+const Gateways = require('./src/workflow/elements/gateways/Gateways')
 
 module.exports = {
   Workflow,
@@ -11,5 +12,6 @@ module.exports = {
     EventListeners,
     EventDispatchers,
     Phases,
+    Gateways,
   },
 }
